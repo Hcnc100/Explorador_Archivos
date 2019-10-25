b1595d5e6ccf87110bea8938ad47b1f8f06b9c82
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2019-10-24 21:30:04 -0500

    Se agregaron todos los comentarios al codigo icons.py

--- icons.py
+++ icons.py
@@ -1,5 +1,8 @@
 class Icono():
-    """docstring forIconos."""
+    """
+    Clase encargada de representar los iconos de un directorio
+    Ademas de guardar referncias en cuanto a la posicion  y el tipo de icono que es
+    """
 
     def __init__(self,dictIcon):
         self.nombre=dictIcon["name"]
@@ -10,21 +13,49 @@ class Icono():
         self.img_select=dictIcon["img_select"]
         self.type=dictIcon["type"]
         self.diametro=35
+
     def dibujar(self,canvas):
-        self.frame=canvas
+        """
+        Metodo que se encaraga de pintar el icono en el canvas
+        :param canvas: Canvas donde se pintaran lo iconos
+        :return:
+        """
+        # se crea un icono anexando una imagen que se presentara cuando se pase el mouse sobre el icono
         self.identificador=canvas.create_image(self.x,self.y,image=self.img,activeimage=self.img_select)
+
+        #se imprime el nombre
         if len(self.nombre)>11:
             canvas.create_text(self.x,self.y+45, text=self.nombre,fill="white")
         else:
             canvas.create_text(self.x,self.y+40, text=self.nombre,fill="white")
-        self.identificadorCuadro=self.frame.create_rectangle(
+
+        #si esta en modo debug se pinta el cuadro que representa los limites sobde se puden dar click a los iconos
+        #esto no afecta el funcionamiento
+        self.identificadorCuadro=canvas.create_rectangle(
 		self.x-self.diametro,
 		self.y-self.diametro,
 		self.x+self.diametro,
         self.y+self.diametro,outline="white")
+
     def setSelect(self):
+        """
+        Funcion setselect , esta funcion se encarga de cambiar el icono por la imagen seleccionada del mismo icoos
+        :return: None
+        """
         self.frame.itemconfigure(self.identificador,image=self.img_select)
     def clearSelect(self):
+        """
+        Funcion clearSelect , esta funcion se encarga de cambiar el icono por el icnono predeterminado
+        :return: None
+        """
         self.frame.itemconfigure(self.identificador,image=self.img)
+
     def contais(self,x,y):
+        """
+        Fuuncion que se basa en una expresion matematica para determinar si las coordenadas dadas por el evento doble click
+        estan dentro del rango del icono
+        :param x: posicion de x del evento doble click
+        :param y:  posicion de y del evento doble click
+        :return: Retorna True o False dependiendo si se encuentra dentro del rango
+        """
         return x>=self.x-self.diametro and y>=self.y-self.diametro and (self.x+self.diametro)-x >=0 and (self.y+self.diametro)-y >=0
